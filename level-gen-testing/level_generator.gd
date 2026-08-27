7409757567ae5e68114501aecf30217452b72ccc
pgcomai <comai.alex@gmail.com>
2026-08-27 14:17:59 -0700

    nothing

--- level-gen-testing/level_generator.gd
+++ level-gen-testing/level_generator.gd
@@ -5,12 +5,17 @@ class_name LevelGenerator
 const DEFAULT_ROOM_SIZE_TILES: Vector2i = Vector2i(20, 11)
 const DEFAULT_ROOM_EMPTY_TILES: Vector2i = Vector2i(16, 7)
 const GEN_TILESET = preload("uid://b0bpeg1j8o6y5")
+const GEN_TILESET_16 = preload("uid://darw0lk61gky0")
 
 
 var gen_seed: int = 0
 var difficulty_factor: float = 1.0
-
+var tilemap_layer := TileMapLayer.new()
+var tilemap_layer_16 := TileMapLayer.new()
 
 
 func _ready() -> void:
-	pass
+	add_child(tilemap_layer)
+	tilemap_layer.tile_set = GEN_TILESET
+	add_child(tilemap_layer_16)
+	tilemap_layer_16.tile_set = GEN_TILESET_16
