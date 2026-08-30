ef34ab8a18be0c17e3bc0d3c4568e02944e4b055
pgcomai <comai.alex@gmail.com>
2026-08-29 17:56:55 -0700

    forgot to get off main

--- Scenes/Player/interactable_area.gd
+++ Scenes/Player/interactable_area.gd
@@ -2,7 +2,19 @@ extends Area2D
 class_name InteractableArea
 
 
+func _ready() -> void:
+	set_collision_layer_and_mask(7, 7)
+
+
 func get_interactable() -> InteractableThing:
 	if get_overlapping_areas():
 		return get_overlapping_areas()[0]
 	return null
+
+
+func set_collision_layer_and_mask(layer: int, mask: int) -> void:
+	for i: int in 16:
+		set_collision_layer_value(i + 1, false)
+		set_collision_mask_value(i + 1, false)
+	set_collision_layer_value(layer, true)
+	set_collision_mask_value(mask, true)
